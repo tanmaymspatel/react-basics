543d1cfcdf39e92ff3169335176f2b910dcf6cb5
Tanmay Patel <tanmay.1rivet@gmail.com>
2022-11-16 19:41:42 +0530

    feat: lazy loading implementation

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import EmployeeForm from '../components/crud/EmployeeForm'
-import EmployeeList from '../components/crud/EmployeeList'
 
-import DataCommunication from '../components/data-communication/DataCommunication'
 import PropsAndState from '../components/data-communication/PropsAndState'
-import FormikForm from '../components/form/FormikForm'
-import Home from '../components/Home'
+const DataCommunication = React.lazy(() => import('../components/data-communication/DataCommunication'))
+const FormikForm = React.lazy(() => import('../components/form/FormikForm'))
+const EmployeeForm = React.lazy(() => import('../components/crud/EmployeeForm'))
+const EmployeeList = React.lazy(() => import('../components/crud/EmployeeList'))
 
 const Routing = () => {
     return (
-        <Routes>
-            <Route path='/' element={<PropsAndState />} />
-            <Route path='/data-communication' element={<DataCommunication />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/form' element={<FormikForm />} />
-            <Route path='/employee-list' element={<EmployeeList />} />
-            <Route path='/employee-form' element={<EmployeeForm />} />
-            <Route path='/edit-emolyee/:id' element={<EmployeeForm />} />
-        </Routes>
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <Routes>
+                <Route path='/' element={<PropsAndState />} />
+                <Route path='/data-communication' element={<DataCommunication />} />
+                <Route path='/form' element={<FormikForm />} />
+                <Route path='/employee-list' element={<EmployeeList />} />
+                <Route path='/employee-form' element={<EmployeeForm />} />
+                <Route path='/edit-emolyee/:id' element={<EmployeeForm />} />
+            </Routes>
+        </Suspense>
     )
 }
 
